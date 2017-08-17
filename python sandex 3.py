53bf3c7bf88e8193de1334f9948da1012c5c5740
rushabhsooni <rushabh.a.soni@gmail.com>
2017-08-17 19:49:35 +0530

    my first changes

---
+++ python sandex 3.py
@@ -0,0 +1,25 @@
+import urllib.request
+import urllib.parse
+
+
+try :
+	url = 'https://www.google.com/search?q=test'
+	#x = urllib.request.urlopen('https://www.google.com/search?q=test')
+	#print (x.read())
+	headers = {}
+	headers['User-Agent'] = 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17'
+	req = urllib.request.Request(url,headers=headers)
+	resp = urllib.request.urlopen(req)
+	respData = resp.read()
+	saveFile = open('text.txt','w')
+	saveFile.write(str(respData))
+	saveFile.close()
+
+
+except Exception as e:
+	print(str(e))
+
+
+#print(respData)
+
+ 
